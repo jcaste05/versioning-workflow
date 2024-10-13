d4db980b0e44911906796306fbadb5c1e2b9c128
Javier <jcaste05@ucm.es>
2024-10-13 11:09:41 +0000

    feat: operation mult added and its test

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,11 @@
 
-from calculator import sum
+from calculator import sum, mult
 
 def test_sum():
     assert sum(2, 3) == 5
     assert sum(-1, 1) == 0
     assert sum(0, 0) == 0
+
+def test_mult():
+    assert mult(2, 3) == 6
+    assert mult(1, 1) == 1
